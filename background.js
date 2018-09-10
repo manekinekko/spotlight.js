a933421443ff93cd15cd5e14164a25a9a86b8229
Wassim Chegham <github@wassimchegham.com>
2018-09-10 11:21:46 +0200

    fix: typo

--- background.js
+++ background.js
@@ -1,6 +1,5 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
-	// for the current tab, inject the "inject.js" file & execute it
-	chrome.tabs.executeScript(tab.ib, {
+	chrome.tabs.executeScript(tab.id, {
 		file: 'index.js'
 	});
 });
